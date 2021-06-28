ec10df9474ae0b379ccf65531be8c4f2aa4a8bb1
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-06-28 19:40:23 +0530

    complete assignment

--- block-BNaabf/server/app.js
+++ block-BNaabf/server/app.js
@@ -2,34 +2,36 @@ const path = require('path');
 const http = require('http');
 const qs = require('querystring');
 const { parse } = require('path');
+const fs = require('fs');
 
 //1.
-console.log('/index.js');
-console.log(path.join(`./${__dirname}`, 'index.js'));
+console.log('../client/index.js');
+console.log(path.join(__dirname, '..', 'client/index.js'));
 //2.
 let server = http.createServer(handleRequest);
 
-function handleRequest(req,res){
-    let store = "";
-    let dataFormat = req.headers['content-type'];
+function handleRequest(req, res) {
+  var store = ';';
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
 
-    req.on('data',(chunk)=>{
-        store += chunk; 
-    })
-
-    req.on('end', () => {
-        if(req.method === 'GET' && dataFormat === 'application/json' && req.url === '/'){
-            res.statusCode = 201;
-            res.end(store);
-        }
-        if(req.method === 'GET' && dataFormat === 'application/x-www-form-urlencoded' && req.url === '/'){
-            res.statusCode = 201;
-            let parsedFormData = qs.parse(store);
-            res.end(parsedFormData.captain);
-        }
-    })
+  req.on('end', () => {
+    if (req.method === 'GET' && req.url === '/form') {
+      res.setHeader('Content-Type', 'text/html');
+      fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    if (req.method === 'POST' && req.url === '/form') {
+      let parsedData = qs.parse(store);
+      console.log(parsedData);
+      res.setHeader('Content-Type', 'text/html');
+      res.end(
+        `<h2>${parsedData.fname}</h2><p>${parsedData.email}</p><span>${parsedData.age}</span>`
+      );
+    }
+  });
 }
 
-server.listen(3333, ()=>{
-    console.log('server is listening at 3333');
-})
+server.listen(5678, () => {
+  console.log('server is listening at 5678');
+});
