56f146db67d199b1c18d4744d0b78d0efdb4a3e6
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-06-28 16:21:50 +0530

    refactor code

--- block-BNaabb/stream.js
+++ block-BNaabb/stream.js
@@ -1,15 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer((req, res)=>{
+http
+  .createServer((req, res) => {
     var store = '';
-    req.on('data',(chunk)=>{
-        store = store + chunk;
-    })
-    req.on('end', ()=>{
-        res.write(store);
-        res.end();
-    })
-}).listen(3456, ()=>{
+    req.on('data', (chunk) => {
+      store = store + chunk;
+    });
+    req.on('end', () => {
+      res.write(store);
+      res.end();
+    });
+  })
+  .listen(3456, () => {
     console.log('Server is listening at 3456');
-})
+  });
